9898f711f15e5a74e2d7b385d162428be5ddd930
Bharadwaz <bharadwazai@gmail.com>
2025-06-23 12:45:28 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,36 @@
+l = [10,20,30,40,50]
+print(l)
+type(l)
+len(l)
+l.append(10)
+l.append(20)
+l.append(30)
+l.append(40)
+l.append(50)
+print(l)
+
+fruits=['apple', 'banana', 'cherry']
+print(fruits)
+print(fruits[0])
+print(fruits[2])
+print(fruits[-3])
+print(fruits[-1])
+fruits.append('date')
+print(fruits)
+fruits.remove('banana')
+print(fruits)
+
+vegitable = ['vankai', 'bendakai', 'sorakai' ]
+print(vegitable)
+vegitable.sort()
+print(vegitable)
+
+king = ['d','c','b', 'a']
+print(king)
+king.sort()
+print(king)
+
+fruit = ['cherry', 'pineapple', 'banana', 'apple', 'fig']
+print(fruit)
+fruit.sort()
+print(fruit)
