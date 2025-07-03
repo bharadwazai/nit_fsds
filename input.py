cf0673d25b8a30a1f6a78dab9fedb99416083f25
Bharadwaz <bharadwazai@gmail.com>
2025-07-03 12:39:26 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,29 @@
+
+
+#x=input()
+#y=input()
+#z=x+y
+#print(z)
+
+#x1=input('distance to office')
+#y1=input('distance from office')
+#z1=x1+y1
+#print(z1)
+
+#print(type(x1))
+#print(type(y1))
+
+#x2=int(input('distance to office : '))
+#y2=int(input('distance from office : 10'))
+#z2=x2+y2
+#print(z2)
+
+#print(type(x2))
+#print(type(y2))
+
+#ch=input('enter a character')
+#print(ch)
+#print(ch[-1])
+
+ch=input('enter a character')[0]
+print(ch)
