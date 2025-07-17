33e0293df3e21d82f5b932950c8e03cb00def6cc
Bharadwaz <bharadwazai@gmail.com>
2025-07-17 13:07:24 +0530

    Add files via upload

---
+++ builtins_.py
@@ -0,0 +1,11 @@
+#Standard
+#Built In
+#No File
+
+#To use the object in the module no need to import.
+#To use the entire module, need to be imported.
+
+#Contains major objects like functions, classes, constants etc. 
+
+import builtins
+print(dir(builtins))
